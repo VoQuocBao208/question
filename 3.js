d8fbb82144c1abb327b4c8e289c9eab480c404b6
unknown <baoquocvo208@gmail.com>
2023-10-18 12:42:42 +0700

    t nho m qua

---
+++ 3.js
@@ -0,0 +1,77 @@
+const myArr = [9,7,9,0,7,8,387,123,456]
+for(let i=0;i <= myArr.length;i++ ){
+    if(myArr[i]%2 == 0 || myArr[i] ==0 ){
+        console.log(myArr[i])
+    }
+   
+}
+
+
+
+
+
+
+
+
+var result = prompt("Em ăn cơm chưa", "Yes");
+if (result != null) {
+    alert("Câu của bạn là: " + result);
+} 
+else {
+    alert("Em chưa trả lời anh");
+}
+
+
+
+
+
+
+
+var result = prompt("Em có muốn ăn bánh ngọt tráng miệng không");
+if (result != null) {
+    alert("Câu của bạn là: " + result);
+ } 
+else {
+    alert("Em chưa trả lời anh");
+ }
+
+
+
+
+
+
+
+var result = prompt("Em đã ăn cơm chưa", "No");
+if (result != null) {
+    alert("Câu của bạn là: " + result);
+ } 
+else {
+    alert("Em chưa trả lời anh");
+ }
+
+
+
+
+
+var result = prompt("Em có muốn ăn cơm với sườn xòa chua ngọt không", "Yes/No");
+if (result != null) {
+    alert("Câu của bạn là: " + result);
+} 
+else {
+    alert("Em chưa trả lời anh");
+}
+
+
+
+
+
+
+
+
+var result = prompt("Em nhớ ăn cơm nha",);
+if (result != null) {
+    alert("Câu của bạn là: " + result);
+} 
+else {
+    alert("Em chưa trả lời anh");
+}
